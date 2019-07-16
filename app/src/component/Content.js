47e6d174a427a7be0876d60c886091f0d9f15f9d
Sergey.Gorshkov <sergey.gorshkov@coreclass.ru>
2019-07-17 00:24:16 +0300

    add style

---
+++ app/src/component/Content.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                Hello, i am Сontent
+            </div>
+        );
+    }
+}
+
+export default Content;
